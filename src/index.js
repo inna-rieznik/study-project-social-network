3979966b3f35d5a4350a84e517c9fe3b7f2dfeb0
inna-rieznik <85076810+inna-rieznik@users.noreply.github.com>
2021-09-19 18:24:14 +0200

    changed state to oop principles

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 //import React from 'react';
 //import ReactDOM from 'react-dom';
 //import './index.css';
-//import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {rerenderTree} from "./render";
-import state from './redux/state';
+//import {rerenderTree} from "./render";
+import store from './redux/state';
+import ReactDOM from "react-dom";
+import {BrowserRouter} from "react-router-dom";
+import App from "./App";
+
+
+export let rerenderTree = () => {
+    ReactDOM.render(
+        <BrowserRouter>
+            {/* like value of atributes we give objectName */}
+            {/* addPostSt and addMessageSt there are without props. and without () becouse of they are called there by import
+       and becouse they are callback functions */}
+            <App state={store.getState()}
+                 addPostSt={store.addPostSt.bind(store)}
+                 addMessageSt={store.addMessageSt.bind(store)}
+                 updateNewPostText={store.updateNewPostText.bind(store)}
+                 updateNewMessageText={store.updateNewMessageText.bind(store)}/>
+        </BrowserRouter>, document.getElementById('root'));
+}
 
 
 
-
-
-
-rerenderTree(state);
+rerenderTree(store.getState());
+store.subscribe(rerenderTree); //callback
 
 reportWebVitals();
 
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,65 +1,78 @@
-import {rerenderTree} from './../render';
+//import {rerenderTree} from './../render';
 
+let store = {
+    _state: {
 
-let state = {
+        profilePage: {
+            posts: [
+                { id: 1, message: "Hey, How you doing?", likes: 10},
+                { id: 2, message: "It's my first post!", likes: 204 },
+                { id: 3, message: "Nice to be there?", likes: 10 }
+            ],
+            newPostText: 'hello'
+        },
 
-    profilePage: {
-        posts: [
-            { id: 1, message: "Hey, How you doing?", likes: 10},
-            { id: 2, message: "It's my first post!", likes: 204 },
-            { id: 3, message: "Nice to be there?", likes: 10 }
-        ],
-        newPostText: 'neconeco'
+        dialogsPage: {
+            dialogs: [
+                { id: 1, name: 'Dima'},
+                { id: 2, name: 'Misha'},
+                { id: 3, name: 'Tania'}
+            ],
+            messages: [
+                { id: 1, message: "Hi" },
+                { id: 2, message: "How you doing" },
+                { id: 3, message: "yo" },
+                { id: 4, message: "yoyoyo" }
+            ],
+            newMessageText: "lalallaaa"
+        }
+    },
+    _callSubscriber() {
+        console.log("state changed");
+    },
+    getState(){
+        return this._state;
+    },
+    addPostSt() {
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likes: 0
+        };
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    addMessageSt() {
+        let newDialogMessage = {
+            id: 1,
+            message: this._state.dialogsPage.newMessageText
+        };
+        this._state.dialogsPage.messages.push(newDialogMessage);
+        this._state.dialogsPage.newMessageText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewMessageText(newText) {
+        this._state.dialogsPage.newMessageText = newText;
+        this._callSubscriber(this._state);
+
     },
 
-    dialogsPage: {
-        dialogs: [
-            { id: 1, name: 'Dima'},
-            { id: 2, name: 'Misha'},
-            { id: 3, name: 'Tania'}
-        ],
-        messages: [
-            { id: 1, message: "Hi" },
-            { id: 2, message: "How you doing" },
-            { id: 3, message: "yo" },
-            { id: 4, message: "yoyoyo" }
-        ],
-        newMessageText: "lalallaaa"
+//subscribe is called -> observer comes; go out of function
+// and looking for the function with the same name. When we fing
+// the finction we initialize for this function observer, so this function
+//is no longer perform her own body, but perform what was get
+// from parameters - OBSERVER
+    subscribe(observer) {
+        this._callSubscriber = observer; //do not use let here
+
     }
 }
 
-export let addPostSt = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likes: 0
-    };
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-     rerenderTree(state);
-}
 
-export let updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderTree(state);
-}
-
-export let addMessageSt = () => {
-    let newDialogMessage = {
-        id: 1,
-        message: state.dialogsPage.newMessageText
-    };
-    state.dialogsPage.messages.push(newDialogMessage);
-    state.dialogsPage.newMessageText = '';
-    rerenderTree(state);
-}
-
-export let updateNewMessageText = (newText) => {
-    state.dialogsPage.newMessageText = newText;
-    rerenderTree(state);
-
-}
-
-
-
-export default state;
+export default store;
+window.store = store;
